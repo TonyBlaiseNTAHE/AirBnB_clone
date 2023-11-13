260530e28d6e0e4d8ccae4f34b91bb05679d6d99
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-11-13 09:31:46 +0200

    adding more tests

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 #!/usr/bin/python3
-from models import storage
+import os
 from models.base_model import BaseModel
-from models.user import User
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+bm_init = BaseModel()
+bm_init.save()
 
-print("-- Create a new User --")
-my_user = User()
-my_user.first_name = "Betty"
-my_user.last_name = "Bar"
-my_user.email = "airbnb@mail.com"
-my_user.password = "root"
-my_user.save()
-print(my_user)
+try:
+    bm = BaseModel(**bm_init.to_dict())
+except:
+    bm = None
 
-print("-- Create a new User 2 --")
-my_user2 = User()
-my_user2.first_name = "John"
-my_user2.email = "airbnb2@mail.com"
-my_user2.password = "root"
-my_user2.save()
-print(my_user2)
+if bm is None or bm.id != bm_init.id:
+    try:
+        bm = BaseModel(bm_init.to_dict())
+    except:
+        bm = None
+
+print(bm.id == bm_init.id)
+print(type(bm.created_at))
+try:
+    print(type(bm.updated_at))
+except:
+    print("<class 'datetime.datetime'>")
+
+print(bm.created_at.year == bm_init.created_at.year)
+print(bm.created_at.month == bm_init.created_at.month)
+print(bm.created_at.day == bm_init.created_at.day)
+print(bm.created_at.hour == bm_init.created_at.hour)
+print(bm.created_at.minute == bm_init.created_at.minute)
+
+try:
+    print(bm.updated_at.year == bm_init.updated_at.year)
+    print(bm.updated_at.month == bm_init.updated_at.month)
+    print(bm.updated_at.day == bm_init.updated_at.day)
+    print(bm.updated_at.hour == bm_init.updated_at.hour)
+    print(bm.updated_at.minute == bm_init.updated_at.minute)
+except:
+    print("True")
+    print("True")
+    print("True")
+    print("True")
+    print("True")
