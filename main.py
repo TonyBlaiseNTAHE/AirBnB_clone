d290e09709a8430ebdca2688a0c7b348fe31c36e
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-11-13 11:09:21 +0200

    fixing errors

--- main.py
+++ main.py
@@ -1,43 +1,92 @@
 #!/usr/bin/python3
+import inspect
+import io
+import sys
+import cmd
+import shutil
+
+"""
+ Cleanup file storage
+"""
 import os
-from models.base_model import BaseModel
-
-bm_init = BaseModel()
-bm_init.save()
-
-try:
-    bm = BaseModel(**bm_init.to_dict())
-except:
-    bm = None
-
-if bm is None or bm.id != bm_init.id:
+file_path = "file.json"
+if not os.path.exists(file_path):
     try:
-        bm = BaseModel(bm_init.to_dict())
+        from models.engine.file_storage import FileStorage
+        file_path = FileStorage._FileStorage__file_path
     except:
-        bm = None
+        pass
+if os.path.exists(file_path):
+    os.remove(file_path)
 
-print(bm.id == bm_init.id)
-print(type(bm.created_at))
-try:
-    print(type(bm.updated_at))
-except:
-    print("<class 'datetime.datetime'>")
 
-print(bm.created_at.year == bm_init.created_at.year)
-print(bm.created_at.month == bm_init.created_at.month)
-print(bm.created_at.day == bm_init.created_at.day)
-print(bm.created_at.hour == bm_init.created_at.hour)
-print(bm.created_at.minute == bm_init.created_at.minute)
+"""
+ Backup console file
+"""
+if os.path.exists("tmp_console_main.py"):
+    shutil.copy("tmp_console_main.py", "console.py")
+shutil.copy("console.py", "tmp_console_main.py")
 
-try:
-    print(bm.updated_at.year == bm_init.updated_at.year)
-    print(bm.updated_at.month == bm_init.updated_at.month)
-    print(bm.updated_at.day == bm_init.updated_at.day)
-    print(bm.updated_at.hour == bm_init.updated_at.hour)
-    print(bm.updated_at.minute == bm_init.updated_at.minute)
-except:
-    print("True")
-    print("True")
-    print("True")
-    print("True")
-    print("True")
+"""
+ Updating console to remove "__main__"
+"""
+with open("tmp_console_main.py", "r") as file_i:
+    console_lines = file_i.readlines()
+    with open("console.py", "w") as file_o:
+        in_main = False
+        for line in console_lines:
+            if "__main__" in line:
+                in_main = True
+            elif in_main:
+                if "cmdloop" not in line:
+                    file_o.write(line.lstrip("    ")) 
+            else:
+                file_o.write(line)
+
+import console
+
+"""
+ Create console
+"""
+console_obj = "HBNBCommand"
+for name, obj in inspect.getmembers(console):
+    if inspect.isclass(obj) and issubclass(obj, cmd.Cmd):
+        console_obj = obj
+
+my_console = console_obj(stdout=io.StringIO(), stdin=io.StringIO())
+my_console.use_rawinput = False
+
+"""
+ Exec command
+"""
+def exec_command(my_console, the_command, last_lines = 1):
+    my_console.stdout = io.StringIO()
+    real_stdout = sys.stdout
+    sys.stdout = my_console.stdout
+    the_command = my_console.precmd(the_command)
+    my_console.onecmd(the_command)
+    sys.stdout = real_stdout
+    lines = my_console.stdout.getvalue().split("\n")
+    return "\n".join(lines[(-1*(last_lines+1)):-1])
+
+"""
+ Tests
+"""
+result = exec_command(my_console, "create User")
+if result is None or result == "":
+    print("FAIL: No ID retrieved")
+    
+with open(file_path, "r") as file:
+    s_file = file.read()
+    if result not in s_file:
+        print("FAIL: New ID not in the JSON file")
+
+model_id = result
+exec_command(my_console, "destroy User {}".format(model_id))
+with open(file_path, "r") as file:
+    s_file = file.read()
+    if result in s_file:
+        print("FAIL: New ID is still in the JSON file")
+print("OK", end="")
+
+shutil.copy("tmp_console_main.py", "console.py")
